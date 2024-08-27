50540d66fa9249a91acad43a392c6d129700a3a3
mudan <myudanov223@gmail.com>
2024-08-27 15:51:05 +0300

    added request for sign-up and response wit token

---
+++ src/main/java/ru/mudan/payload/request/SignUpRequest.java
@@ -0,0 +1,15 @@
+package ru.mudan.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpRequest {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+}
