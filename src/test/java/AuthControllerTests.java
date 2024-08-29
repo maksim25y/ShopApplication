b78905cbbae2f70809ce1ae363b1f9d6e3398117
mudan <myudanov223@gmail.com>
2024-08-29 14:30:26 +0300

    added test with bland values and values that equals null and email that already exists

--- src/test/java/AuthControllerTests.java
+++ src/test/java/AuthControllerTests.java
@@ -42,8 +42,6 @@ public class AuthControllerTests {
     private ResponseErrorValidation responseErrorValidation;
     @Mock
     private SignUpValidator signUpValidator;
-    @Mock
-    private BindingResult bindingResult;
     @InjectMocks
     private AuthController authController;
     private MockMvc mockMvc;
@@ -132,6 +130,225 @@ public class AuthControllerTests {
         String message = getMessageFromJSOM(responseBody, "username");
 
         assertEquals("Пользователь с данным username уже существует",message);
+    }
+    @Test
+    public void whenRegisterUserWithFirstnameEqualsNullReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setFirstname(null);
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("firstname","Имя не должно быть пустым или состоять только из пробелов");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "firstname");
+
+        assertEquals("Имя не должно быть пустым или состоять только из пробелов", message);
+    }
+    @Test
+    public void whenRegisterUserWithFirstnameIsBlankReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setFirstname("");
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("firstname","Имя не должно быть пустым или состоять только из пробелов");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "firstname");
+
+        assertEquals("Имя не должно быть пустым или состоять только из пробелов", message);
+    }
+    @Test
+    public void whenRegisterUserWithLastnameEqualsNullReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setLastname(null);
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("lastname","Фамилия не должна быть пустой или состоять только из пробелов");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "lastname");
+
+        assertEquals("Фамилия не должна быть пустой или состоять только из пробелов", message);
+    }
+    @Test
+    public void whenRegisterUserWithLastnameIsBlankReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setLastname("");
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("lastname","Фамилия не должна быть пустой или состоять только из пробелов");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "lastname");
+
+        assertEquals("Фамилия не должна быть пустой или состоять только из пробелов", message);
+    }
+    @Test
+    public void whenRegisterUserWithEmailEqualsNullReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setEmail(null);
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("email","email не должен быть пустым или состоять только из пробелов");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "email");
+
+        assertEquals("email не должен быть пустым или состоять только из пробелов", message);
+    }
+    @Test
+    public void whenRegisterUserWithEmailIsBlankReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setEmail("");
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("email","email не должен быть пустым или состоять только из пробелов");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "email");
+
+        assertEquals("email не должен быть пустым или состоять только из пробелов", message);
+    }
+    @Test
+    public void whenRegisterUserWithEmailThatAlreadyExistsReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("email","Пользователь с данным email уже существует");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "email");
+
+        assertEquals("Пользователь с данным email уже существует", message);
+    }
+    @Test
+    public void whenRegisterUserWithPasswordEqualsNullReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setPassword(null);
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("password","Пароль не должен быть пустым или состоять только из пробелов");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "password");
+
+        assertEquals("Пароль не должен быть пустым или состоять только из пробелов", message);
+    }
+    @Test
+    public void whenRegisterUserWithPasswordIsBlankReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setPassword("");
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("password","Пароль не должен быть пустым или состоять только из пробелов");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "password");
+
+        assertEquals("Пароль не должен быть пустым или состоять только из пробелов", message);
+    }
+    @Test
+    public void whenRegisterUserWithUsernameWithLength5ReturnMessageThatRegisterIsBad() throws Exception {
+        SignUpRequest signUpRequest = getTestSignUpRequest();
+        signUpRequest.setUsername("Тест");
+        String requestContent = serializeToJson(signUpRequest);
+
+        Map<String,String>expectedResponse = Map.of("username","username должен быть от 6 до 15 символов длиной");
+        ResponseEntity<Object>responseEntity = new ResponseEntity<>(expectedResponse,HttpStatus.BAD_REQUEST);
+
+        when(responseErrorValidation.mapValidationService(any(BindingResult.class))).thenReturn(responseEntity);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(AUTH_URL + "/sign-up")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestContent))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+
+        String responseBody = getResponseBodyAsString(result);
+        String message = getMessageFromJSOM(responseBody, "username");
+
+        assertEquals("username должен быть от 6 до 15 символов длиной", message);
     }
     private String getMessageFromJSOM(String responseBody,String path){
         return JsonPath.read(responseBody, String.format("$.%s",path));
